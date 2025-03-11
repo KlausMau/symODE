a451530141ba53228a7db7947dbcb81ae73120be
KlausMau <erik.th.k.mau@gmail.com>
2025-03-11 20:50:05 +0100

    add utility modules

---
+++ src/componentwise_expression.py
@@ -0,0 +1,64 @@
+'''This module contains the class ComponentwiseExpression, which is used to represent an expression
+as a sum of components.'''
+
+import sympy as sy
+from sympy.simplify.fu import TR10
+
+def get_coefficients_of_trigonometric_expression(equation : sy.Expr,
+                                                 variable: sy.Symbol,
+                                                 order_of_trigonometrics: int):
+    '''returns the coefficients of an expression with sin and cos'''
+    # transform products of sin and cos to sums of sin and cos
+    equation = TR10(equation)
+
+    # replace sin/cos terms by exponential of dummy variable
+    exp_dummy = sy.symbols('exp_dummy')
+    equation = equation.replace(sy.cos(variable), (exp_dummy + exp_dummy**-1)/2)
+    equation = equation.replace(sy.sin(variable), (exp_dummy - exp_dummy**-1)/(2*sy.I))
+
+    # collect coefficients
+    real_coefficients = []
+    complex_coefficients = sy.Poly(equation*exp_dummy**order_of_trigonometrics, exp_dummy).all_coeffs()
+
+    for i in range(order_of_trigonometrics):
+        real_coefficients.append(sy.re(complex_coefficients[i]))
+        real_coefficients.append(sy.im(complex_coefficients[i]))
+
+    real_coefficients.append(sy.re(complex_coefficients[order_of_trigonometrics]))
+    return real_coefficients
+
+def get_coefficients_of_polynomial_expression(polynomial: sy.Expr, variable: sy.Expr,
+                                              carry: sy.Expr) -> dict[sy.Expr, sy.Expr]:
+    coefficients = sy.Poly(polynomial, variable).all_coeffs()
+    maximum_power = len(coefficients)
+    return {carry*variable**(maximum_power-power-1): coeff for power, coeff in enumerate(coefficients)}
+
+class ComponentwiseExpression:
+
+    def __init__(self, expression: sy.Expr) -> None:
+        self._expression = {1: expression}
+
+    def split(self, split_term: sy.Expr) -> None:
+        new_components = {}
+        for component, term in self._expression.items():
+             new_components.update({**get_coefficients_of_polynomial_expression(term, split_term, component)})
+        self._expression = new_components
+
+    def prune(self) -> None:
+        self._expression = {component: term for component, term in self._expression.items()
+                            if term != 0 and component != 0}
+
+    def sum_up(self) -> sy.Expr:
+        return sum([component*term for component, term in self._expression.items()])
+
+    def subs(self, substitutions: dict[sy.Expr, sy.Expr]) -> None:
+        self._expression = {component.subs(substitutions): term.subs(substitutions).cancel()
+                            for component, term in self._expression.items()}
+
+    def show(self, number_of_ops = None) -> None:
+        for component, term in self._expression.items():
+            if number_of_ops is None:
+                print(f'{component}: {term}')
+            else:
+                if sy.count_ops(term) <= number_of_ops:
+                    print(f'{component}: {term}')
---
+++ src/util.py
@@ -0,0 +1,75 @@
+'''This module contains utility functions.'''
+
+import sympy as sy
+from src.dynamical_system import DynamicalSystem
+from src.componentwise_expression import ComponentwiseExpression
+
+def update_solution(solution: dict[sy.Symbol, sy.Expr],
+                    new_solution_part: dict[sy.Symbol, sy.Expr]) -> dict[sy.Symbol, sy.Expr]:
+    '''returns a solution updated with new information'''
+    for parameter, value in solution.items():
+        solution[parameter] = value.subs(new_solution_part).cancel()
+
+    solution.update(new_solution_part)
+
+    return solution
+
+def find_solution_of_equation_by_inserting_values(equation : sy.Expr, variable: sy.Symbol,
+                                                  value_parameter_list: dict[sy.Symbol, sy.Expr],
+                                                  show_process : bool =False):
+    '''returns the solution of the equation by inserting the given values'''
+    solutions : dict[sy.Symbol, sy.Expr] = {}
+
+    for solvable_parameter, variable_value in value_parameter_list.items():
+        sol = sy.solve(equation.subs({variable: variable_value}), solvable_parameter)
+
+        if not sol:
+            print(f'No solution found for {solvable_parameter}')
+            return solutions
+
+        solved_parameter_expression = sol[0]
+
+        if show_process is True:
+            print(f'{solvable_parameter}={solved_parameter_expression}')
+
+        new_solution_part = {solvable_parameter: solved_parameter_expression.simplify()}
+
+        # update the solutions
+        solutions = update_solution(solutions, new_solution_part)
+
+        # update equation
+        equation = equation.subs(new_solution_part)
+
+    # check whether the solution is complete (complete = prints "0")
+    if show_process is True:
+        print('remaining terms of equation:')
+        print(equation.simplify())
+
+    return solutions
+
+def get_remainder_with_rational_ansatz(system: DynamicalSystem, numerator: sy.Expr, denominator: sy.Expr, ld: sy.Symbol) -> tuple[sy.Expr, ComponentwiseExpression]:
+    dt_numerator = system.get_time_derivative_of_observable(numerator)
+    dt_denominator = system.get_time_derivative_of_observable(denominator)
+
+    equation = dt_numerator*denominator - dt_denominator*numerator - ld*denominator*numerator
+    observable = numerator/denominator
+
+    return observable, ComponentwiseExpression(equation)
+
+def get_remainder_with_exponential_ansatz(system: DynamicalSystem, factor: sy.Expr, exponent: sy.Expr, ld: sy.Symbol) -> tuple[sy.Expr, ComponentwiseExpression]:
+    dt_factor = system.get_time_derivative_of_observable(factor)
+    dt_exponent = system.get_time_derivative_of_observable(exponent)
+
+    equation = dt_factor - dt_exponent*factor - ld*factor
+    observable = factor*sy.exp(exponent)
+
+    return observable, ComponentwiseExpression(equation)
+
+def get_remainder_with_complex_ansatz(system: DynamicalSystem, complex_polynomial: sy.Expr, real_polynomial: sy.Expr, ld: sy.Symbol, beta: sy.Symbol) -> tuple[sy.Expr, ComponentwiseExpression]:
+    dt_complex_polynomial = system.get_time_derivative_of_observable(complex_polynomial)
+    dt_real_polynomial = system.get_time_derivative_of_observable(real_polynomial)
+
+    equation = dt_complex_polynomial*real_polynomial + beta*dt_real_polynomial*complex_polynomial - ld*complex_polynomial*real_polynomial
+    observable = complex_polynomial*sy.exp(beta*sy.log(real_polynomial))
+
+    return observable, ComponentwiseExpression(equation)
